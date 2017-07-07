f170d6e442b6bd76303e4193cb0df9a7950c7862
NicodeLee <lirizhilirizhi@163.com>
2017-07-07 11:40:12 +0800

    用视频第二天的配置

--- init.el
+++ init.el
@@ -1,142 +1,135 @@
+
 ;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
+;; installed packages.  Don't delete this line.  If you don't want (setq  initial-frame-alist (quote ((fullscreen . maximized))))it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 
- (when (>= emacs-major-version 24)
-     (require 'package)
-     (package-initialize)
-     (setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
-                      ("melpa" . "http://elpa.emacs-china.org/melpa/"))))
+(when (>= emacs-major-version 24)
+    (require 'package)
+    (package-initialize)
+    (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+    )
 
-;; 注意 elpa.emacs-china.org 是 Emacs China 中文社区在国内搭建的一个 ELPA 镜像
+(require 'cl)
 
- ;; cl - Common Lisp Extension
- (require 'cl)
+;;add whatever packages you want here
+(defvar zilongshanren/packages '(
+				   company
+				   monokai-theme
+				   hungry-delete
+				   swiper
+				   counsel
+				   smartparens
+				   js2-mode
+				   nodejs-repl
+				   exec-path-from-shell
+				   
+				   )  "Default packages")
 
- ;; Add Packages
- (defvar my/packages '(
-                ;; --- Auto-completion ---
-                company
-                ;; --- Better Editor ---
-                hungry-delete
-                swiper
-                counsel
-                smartparens
-                ;; --- Major Mode ---
-                js2-mode
-                ;; --- Minor Mode ---
-                nodejs-repl
-                exec-path-from-shell
-                ;; --- Themes ---
-                monokai-theme
-                ;; solarized-theme
-                ) "Default packages")
+(setq package-selected-packages zilongshanren/packages)
 
- (setq package-selected-packages my/packages)
+(defun zilongshanren/packages-installed-p ()
+    (loop for pkg in zilongshanren/packages
+          when (not (package-installed-p pkg)) do (return nil)
+          finally (return t)))
 
- (defun my/packages-installed-p ()
-     (loop for pkg in my/packages
-           when (not (package-installed-p pkg)) do (return nil)
-           finally (return t)))
+(unless (zilongshanren/packages-installed-p)
+    (message "%s" "Refreshing package database...")
+    (package-refresh-contents)
+    (dolist (pkg zilongshanren/packages)
+      (when (not (package-installed-p pkg))
+        (package-install pkg))))
 
- (unless (my/packages-installed-p)
-     (message "%s" "Refreshing package database...")
-     (package-refresh-contents)
-     (dolist (pkg my/packages)
-       (when (not (package-installed-p pkg))
-         (package-install pkg))))
+;; let emacs could find the execuable
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
 
- ;; Find Executable Path on OS X
- (when (memq window-system '(mac ns))
-   (exec-path-from-shell-initialize))
+(require 'hungry-delete)
+(global-hungry-delete-mode)
 
-;;(package-initialize)
+(require 'smartparens-config)
+;;(add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
+(smartparens-global-mode t)
 
-;; 关闭工具栏，tool-bar-mode 即为一个 Minor Mode
+;; config js2-mode for js files
+(setq auto-mode-alist
+	  (append
+	   '(("\\.js\\'" . js2-mode))
+	   auto-mode-alist))
+
+
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "C-h f") 'counsel-describe-function)
+(global-set-key (kbd "C-h v") 'counsel-describe-variable)
+
+
 (tool-bar-mode -1)
+(scroll-bar-mode -1)
 
-;; 关闭文件滑动控件
-(scroll-bar-mode 1)
+(setq inhibit-splash-screen t)
 
-;; 显示行号
-(global-linum-mode 1)
+(global-linum-mode t)
 
-;; 更改光标的样式（不能生效，解决方案见第二集）
-(setq cursor-type 'bar)
-
-;; 关闭启动帮助画面
-(setq inhibit-splash-screen 1)
-
-;; 关闭缩进 (第二天中被去除)
-;; (electric-indent-mode -1)
-
-;; 更改显示字体大小 16pt
-;; http://stackoverflow.com/questions/294664/how-to-set-the-font-size-in-emacs
-(set-face-attribute 'default nil :height 160)
-
-;; 快速打开配置文件
-(defun open-init-file()
+(defun open-my-init-file()
   (interactive)
   (find-file "~/.emacs.d/init.el"))
 
-;; 这一行代码，将函数 open-init-file 绑定到 <f2> 键上
-(global-set-key (kbd "<f2>") 'open-init-file)
+(global-set-key (kbd "<f2>") 'open-my-init-file)
 
-;; 关闭工具栏，tool-bar-mode 即为一个 Minor Mode
-(tool-bar-mode -1)
+(global-company-mode t)
+(setq-default cursor-type 'bar)
 
-;; 关闭文件滑动控件
-(scroll-bar-mode -1)
+(setq make-backup-files nil)
 
-;; 显示行号
-(global-linum-mode 1)
 
-;; 更改光标的样式（不能生效，解决方案见第二集）
-(setq cursor-type 'bar)
+(require 'org)
 
-;; 关闭启动帮助画面
-(setq inhibit-splash-screen 1)
-
-;; 关闭缩进 (第二天中被去除)
-;; (electric-indent-mode -1)
+(setq org-src-fontify-natively t)
 
 ;; 更改显示字体大小 14pt
 ;; http://stackoverflow.com/questions/294664/how-to-set-the-font-size-in-emacs
 (set-face-attribute 'default nil :height 140)
 
-;; 快速打开配置文件
-(defun open-init-file()
-  (interactive)
-  (find-file "~/.emacs.d/init.el"))
-
-;; 这一行代码，将函数 open-init-file 绑定到 <f2> 键上
-(global-set-key (kbd "<f2>") 'open-init-file)
-
-;; 开启全局 Company 补全
-(global-company-mode 1)
-
 (require 'recentf)
-(recentf-mode 1)
-(setq recentf-max-menu-item 10)
+(recentf-mode 1)			
+(setq recentf-max-menu-items 25)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
 
-;; 这个快捷键绑定可以用之后的插件 counsel 代替
- (global-set-key (kbd "C-x C-r") 'recentf-open-files)
+(delete-selection-mode t)
 
-(delete-selection-mode 1)
+;;(setq  initial-frame-alist (quote ((fullscreen . maximized))))
 
-(setq make-backup-files nil)
-
-(delete-selection-mode 1)
-
-;;将 Emacs 设置为开启默认全屏
-;;(setq initial-frame-alist (quote ((fullscreen . maximized))))
-
-;;自动括号匹配
 (add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
 
-;;安装主题
-;;(add-to-list my/packages 'monokai-theme)
+(global-hl-line-mode t)
 
-;;每次打开编辑器时加载主题
-(load-theme 'monokai 1)
+(load-theme 'monokai t)
+
+(global-set-key (kbd "C-h C-f") 'find-function)
+(global-set-key (kbd "C-h C-v") 'find-variable)
+(global-set-key (kbd "C-h C-k") 'find-function-on-key)
+
+ (setq org-agenda-files '("~/org"))
+  (global-set-key (kbd "C-c a") 'org-agenda)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-idle-delay 0.08)
+ '(company-minimum-prefix-length 1)
+ '(custom-safe-themes
+   (quote
+    ("6c62b1cd715d26eb5aa53843ed9a54fc2b0d7c5e0f5118d4efafa13d7715c56e" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(js2-external-variable ((t (:foreground "dark gray")))))
